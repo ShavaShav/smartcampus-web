e85ba539e850702c84ab36f5a4b1be4085991169
Zach Shaver <shaverz@uwindsor.ca>
2018-04-26 23:42:18 -0400

    Add fetch() wrapper for laravel backend
    Add wrapper for fetch api to use laravel backend.
    It gets the api url from the env var REACT_APP_API_URL.
    
    Promise returned by apiFetch() resolves to an object like:
    {
      "status": 200,
      "body": {
        "message": "Ok"
      }
    }
    The body being the json response from the api.

---
+++ src/api.js
@@ -0,0 +1,86 @@
+const API_ROOT = `${process.env.REACT_APP_API_URL}/api`;
+
+/**
+ * Calls SmartCampus API with proper options and formats response
+ * @param  {String}  path     Endpoint to hit (e.g. '/events')
+ * @param  {String}  method   HTTP verb (e.g. 'GET')
+ * @param  {Object}  body     JSON request body
+ * @return {Promise(Object)}  Response containing 'status' and 'body'
+ */
+const apiFetch = (path, method = 'GET', body = null) => {
+
+  const options = {
+    method,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  };
+
+  // Add POST/PUT body, if given
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+
+  // Add token as header, if in storage
+  const token = localStorage.getItem('token');
+  if (token) {
+    options.headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  // Call the API
+  return fetch(`${API_ROOT}${path}`, options).then(response => {
+    return response.json().then(json => {
+      
+      // Wrap response json body with status code
+      const status_json = {
+        status: response.status,
+        body: json
+      };
+
+      // Reject promise if bad response
+      return response.ok ? status_json : Promise.reject(status_json);
+    });
+  });
+};
+
+//===================================================================
+// Request Helpers
+//
+
+const requests = {
+  del: path =>
+    apiFetch(path, 'DELETE'),
+  get: path =>
+    apiFetch(path, 'GET'),
+  put: (path, body) =>
+    apiFetch(path, 'PUT', body),
+  post: (path, body) =>
+    apiFetch(path, 'POST', body)
+};
+
+const User = {
+  current: () =>
+    requests.get('/user'),
+  login: (email, password) =>
+    requests.post('/user/login', { user: { email, password } }),
+  logout: () =>
+    requests.post('/user/logout'),
+  register: (username, email, password) =>
+    requests.post('/user/register', { user: { username, email, password } })
+};
+
+const Event = {
+  create: (title, time, location, link, body) =>
+    requests.post('/events', { event: { title, time, location, link, body } }),
+  delete: (id) =>
+    requests.del('/events/' + id),
+  get: (id) =>
+    requests.get('/events/' + id),
+  feed: () =>
+    requests.get('/events')
+};
+
+export default {
+  User, Event
+};
